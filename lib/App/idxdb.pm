65edc4c382d7bb430f39d3a4bd4aa1bbead91374
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-17 21:13:12 +0700

    Import stock ownership data from KSEI text files to database

--- lib/App/idxdb.pm
+++ lib/App/idxdb.pm
@@ -138,6 +138,7 @@ sub update {
                     $dbh->do("CREATE TABLE daily_trading_summary (".join(", ", @field_defs).")");
                     $dbh->do("CREATE INDEX ix_daily_trading_summary__StockCode ON daily_trading_summary(StockCode)");
                     $dbh->do("CREATE UNIQUE INDEX ix_daily_trading_summary__Date__StockCode ON daily_trading_summary(Date,StockCode)");
+                    $table_exists++;
                 }
 
                 my $sth_sel_stock = $dbh->prepare("SELECT code FROM stock WHERE code=?");
@@ -156,6 +157,92 @@ sub update {
                     $row->{ctime} = time();
                     $row->{mtime} = time();
                     $sth_ins_daily_trading_summary->execute((map { $row->{$_} } @table_fields));
+                }
+                $dbh->commit;
+            }
+        }
+    } # UPDATE_DAILY_TRADING_SUMMARY
+
+  UPDATE_OWNERSHIP:
+    {
+        my $table_exists = DBIx::Util::Schema::table_exists($dbh, 'stock_ownership');
+        my @table_fields;
+        if ($table_exists) {
+            @table_fields = map { $_->{COLUMN_NAME} } DBIx::Util::Schema::list_columns($dbh, 'stock_ownership');
+        }
+        local $CWD = "$gd_path/table/ksei_sec_ownership/raw";
+      YEAR:
+        for my $year (reverse glob("*")) {
+            local $CWD = $year;
+          YEARMON:
+            for my $yearmon (reverse glob("*")) {
+                local $CWD = $yearmon;
+                my @txt_files = glob("*.txt");
+                unless (@txt_files) {
+                    log_debug "Directory $CWD does not contain any .txt files, skipping";
+                    next YEARMON;
+                }
+
+                my $filename = $txt_files[0];
+                $filename =~ /(\d{4})(\d{2})(\d{2})\./ or die;
+                log_trace "Processing file $CWD/$filename ...";
+                my $date = "$1-$2-${3}";
+                if ($table_exists && $dbh->selectrow_array(q(SELECT 1 FROM stock_ownership WHERE "date" = ?), {}, $date)) {
+                    log_debug "Data for date $date already exist, skipping this date";
+                    next YEARMON;
+                }
+                open my $fh, "<", $filename or die "Can't open $filename: $!";
+                chomp(my $line = <$fh>);
+                my @fields = split /\|/, $line;
+                for my $f (@fields) {
+                    $f =~ s/[^A-Za-z]+//g;
+                    if ($f eq 'Total') {
+                        # there are two Total columns, the first one is local
+                        # total, the second one is foreign total.
+                        if (grep { $_ eq 'LocalTotal'} @fields) {
+                            $f = 'ForeignTotal';
+                        } else {
+                            $f = 'LocalTotal';
+                        }
+                    }
+                }
+
+                unless ($table_exists) {
+                    my @table_field_defs;
+                    push @table_fields    , "date";
+                    push @table_field_defs, "date TEXT NOT NULL";
+                    for my $f (@fields) {
+                        next if $f =~ /^(Date|Type|SecNum)$/;
+                        my $type;
+                        $type = 'TEXT' if $f =~ /^(Code)$/;
+                        $type //= 'DECIMAL';
+                        push @table_fields, $f;
+                        push @table_field_defs, qq("$f" $type);
+                    }
+                    push @table_fields    , "ctime", "mtime";
+                    push @table_field_defs, "ctime INT NOT NULL", "mtime INT NOT NULL";
+                    my $sql = "CREATE TABLE stock_ownership (".join(", ", @table_field_defs).")";
+                    #log_warn $sql;
+                    $dbh->do($sql);
+                    $dbh->do("CREATE INDEX ix_stock_ownership__Code ON stock_ownership(Code)");
+                    $dbh->do("CREATE UNIQUE INDEX ix_stock_ownership__date__Code ON stock_ownership(date,Code)");
+                    $table_exists++;
+                }
+
+                my $sql = "INSERT INTO stock_ownership (".join(",", map {qq("$_")} @table_fields).") VALUES (".join(",", map {"?"} @table_fields).")";
+                #log_warn $sql;
+                my $sth_ins_stock_ownership = $dbh->prepare($sql);
+                $dbh->begin_work;
+                while (my $line = <$fh>) {
+                    chomp($line);
+                    my @row = split /\|/, $line;
+                    my $row = {};
+                    for (0..$#fields) { $row->{ $fields[$_] } = $row[ $_ ] }
+                    next unless $row->{Type} eq 'EQUITY';
+                    $row->{date}  = $date;
+                    $row->{ctime} = time();
+                    $row->{mtime} = time();
+                    $sth_ins_stock_ownership->execute((map { $row->{$_} } @table_fields));
                 }
                 $dbh->commit;
             }
